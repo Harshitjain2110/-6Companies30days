afc5df4ae7cf2cb6e120729fa395988dd9e720c1
Harshit Jain <77147128+Harshitjain2110@users.noreply.github.com>
2022-01-09 22:11:56 +0530

    Create Greatest Common  Divisor of Strings.cpp

---
+++ Goldman Sachs/Greatest Common  Divisor of Strings.cpp
@@ -0,0 +1,46 @@
+/*
+For two strings s and t, we say "t divides s" if and only if s = t + ... + t (i.e., t is concatenated with itself one or more times).
+
+Given two strings str1 and str2, return the largest string x such that x divides both str1 and str2.
+
+ 
+
+Example 1:
+
+Input: str1 = "ABCABC", str2 = "ABC"
+Output: "ABC"
+Example 2:
+
+Input: str1 = "ABABAB", str2 = "ABAB"
+Output: "AB"
+Example 3:
+
+Input: str1 = "LEET", str2 = "CODE"
+Output: ""
+ 
+
+Constraints:
+
+1 <= str1.length, str2.length <= 1000
+str1 and str2 consist of English uppercase letters.
+*/
+
+
+
+//__gcd(num1,num2) --STL FUNCTION TO FIND GCD OF 2 NUMBERS.
+class Solution {
+public:
+    string gcdOfStrings(string str1, string str2) {
+        string ans ="";
+        
+        if(str1+str2 == str2+str1){
+            int len = __gcd(str1.length(),str2.length());
+            
+            if(str1.length()<str2.length())
+                ans+=str1.substr(0,len);
+            else
+                ans+=str2.substr(0,len);
+        }
+        return ans; 
+    }
+};
